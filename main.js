a09856724c17cbb51d4395a25a45affac3af5da9
Sibuuu07 <sarbeswarbhol754@gmail.com>
2022-10-16 16:29:21 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log('Hello World!');
+fetch('./api.json')
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json)
+        let data = "";
+        json.map((values) => {
+            data += `<tr>
+                          <td></td>
+                          <td>${values.API}</td>
+                          <td>${values.Category}</td>
+                          <td>${values.Description}</td>
+                          <td><a href="${values.Link}" style="text-decoration: none" target="_blank">${values.Link}</a></td>
+                          <td>${values.HTTPS}</td>
+                        </tr>
+                    `
+        })
+        console.log(data);
+        document.getElementById("data_show").innerHTML = data;
+    }).catch((err) => document.write("can't fetch data"))
